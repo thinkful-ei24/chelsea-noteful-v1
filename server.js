bf73ba8b195c4c8a0f7c6313b4af0acf8a03a08c
clkent <chelsea.kent@gmail.com>
2018-09-04 12:36:02 -0700

    search

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+// return notes app & check for search query
 app.get('/api/notes', (req, res) => {
+  let { searchTerm } = req.query;
+  if (searchTerm) {
+    return res.json(data.filter(item => item.title.includes(searchTerm)));
+  }
   res.json(data);
 });
 
+// return specific note in notes app
 app.get('/api/notes/:id', (req, res) => {
   //written out way
   // let id = req.params.id;
