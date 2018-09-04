b4a336a4897c6555c1d8730a17f03f08700fe5b0
clkent <chelsea.kent@gmail.com>
2018-09-04 11:59:20 -0700

    get note details endpoint

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  //written out way
+  // let id = req.params.id;
+  // short hand
+  let { id } = req.params;
+  res.json(data.find(item => item.id === Number(id)));
+});
+
 app
   .listen(8080, function() {
     console.info(`Server listening on ${this.address().port}`);
