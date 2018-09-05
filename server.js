e80c9a291ea4b9f23df20c31d659d3dc39a1b665
clkent <chelsea.kent@gmail.com>
2018-09-05 12:24:28 -0700

    error handler

--- server.js
+++ server.js
@@ -31,6 +31,27 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === Number(id)));
 });
 
+//test
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+// check error
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+// Error handling
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
 app
   .listen(PORT, function() {
     console.info(`Server listening on ${this.address().port}`);
